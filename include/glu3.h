502c04f6fd3cd8941b9c66134d28a908bf29016c
Ian Romanick <idr@freedesktop.org>
2010-07-21 13:09:08 -0700

    Work around systems that don't have stdbool.h

--- include/glu3.h
+++ include/glu3.h
@@ -37,7 +37,21 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 
-#include <stdbool.h>
+#ifdef HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# ifndef HAVE__BOOL
+#  ifdef __cplusplus
+typedef bool _Bool;
+#  else
+#   define _Bool signed char
+#  endif
+# endif
+# define bool _Bool
+# define false 0
+# define true 1
+# define __bool_true_false_are_defined 1
+#endif
 
 #define GLU3_VERSION_0_1
 #define GLU3_VERSION_0_9
--- src/system.h
+++ src/system.h
@@ -5,19 +5,3 @@
 #ifndef HAVE_STRTOF
 #define strtof(_a, _b) ((float) strtod(_a, _b))
 #endif
-
-#ifdef HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# ifndef HAVE__BOOL
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  else
-#   define _Bool signed char
-#  endif
-# endif
-# define bool _Bool
-# define false 0
-# define true 1
-# define __bool_true_false_are_defined 1
-#endif
